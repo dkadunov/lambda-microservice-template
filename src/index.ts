b2073442d5596d765eef4c1a5051dd0c3f89641b
dkadunov <dkadunov@gmail.com>
2018-04-27 23:24:39 -0400

    sample lambda code

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+'user strict'
+
+import { Callback, Context, Handler } from 'aws-lambda';
+import { pathOr } from 'ramda';
+
+export const handler: Handler = (event, context: Context, callback: Callback) => {
+    console.log('externalsd test => ' + pathOr('good', ['noneExisting'], {}));
+
+    callback(null, 'good stuff');
+}
