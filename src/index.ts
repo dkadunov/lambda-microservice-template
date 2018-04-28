5b6bac46f9ccbb0d9eaf3b68efd8ef67c86327e2
Dmitriy Kadunov <dkadunov@gmail.com>
2018-04-28 01:37:02 -0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { Callback, Context, Handler } from 'aws-lambda';
 import { pathOr } from 'ramda';
 
 export const handler: Handler = (event, context: Context, callback: Callback) => {
-    console.log('externalsd test => ' + pathOr('good', ['noneExisting'], {}));
+    console.log('externals test => ' + pathOr('good', ['noneExisting'], {}));
 
     callback(null, 'good stuff');
 }
